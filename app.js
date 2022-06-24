f16df29d790953175f8d4a81d31fedd27eef261a
laboo25 <89362256+laboo25@users.noreply.github.com>
2022-06-24 20:58:02 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        // dots: false,
+    // infinite: false,
+//     autoplay: true,
+//   autoplaySpeed: 1000,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1
+    });
+  });
